0d0b05b70f104d233ea6d66abe61954cbca2db67
AkshayB <akshay.bodhankar@incalus.com>
2026-04-29 15:42:35 +0530

    Added to do form

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask, jsonify
-import json 
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# API route
+client = MongoClient('mongodb://localhost:27017/')
+db = client['student_db']
+collection = db['students']
 
-@app.route('/api', methods=['GET'])
-def get_data():
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-    return jsonify(data)
+@app.route('/')
+def form():
+    return render_template('form.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    try:
+        name = request.form['name']
+        age = int(request.form['age'])
+
+        collection.insert_one({
+            'name': name, 
+            'age': age
+        })
+        return redirect(url_for("success"))
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return render_template('form.html', error=str(e))
+    
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
