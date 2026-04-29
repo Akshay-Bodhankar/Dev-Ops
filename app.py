d2dd47d6afddcfcd3b2bf389efe0c234d37800ef
AkshayB <akshay.bodhankar@incalus.com>
2026-04-29 16:02:49 +0530

    Recommit after reset to Item ID state

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# MongoDB connection
 client = MongoClient('mongodb://localhost:27017/')
-db = client['student_db']
-collection = db['students']
+db = client['todo_db']
+collection = db['todo_items']
 
 @app.route('/')
 def form():
