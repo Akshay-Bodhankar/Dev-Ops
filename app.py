d397984f15b3bd807e5adc514c465b41801a54ea
AkshayB <akshay.bodhankar@incalus.com>
2026-04-29 15:25:59 +0530

    updated json data

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
+from flask import Flask, jsonify
+import json 
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['student_db']
-collection = db['students']
+# API route
 
-@app.route('/')
-def form():
-    return render_template('form.html')
+@app.route('/api', methods=['GET'])
+def get_data():
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    return jsonify(data)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    try:
-        name = request.form['name']
-        age = int(request.form['age'])
-
-        collection.insert_one({
-            'name': name, 
-            'age': age
-        })
-        return redirect(url_for("success"))
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return render_template('form.html', error=str(e))
-    
-@app.route('/success')
-def success():
-    return render_template('success.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/incalus/Desktop/devops-assignment/Dev-Ops-Assig/flask-mongo/que2/venv
+setenv VIRTUAL_ENV /home/incalus/Desktop/devops-assignment/Dev-Ops-Assig/flask-mongo/que1/venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
